a5aff57ccae9bfd754e02e45ed089cc01a809df8
Humberto Frizzera <dj1berto@gmail.com>
2023-08-21 19:50:12 -0300

    criação de projeto

---
+++ src/main/java/com/hfsystems/hallmark/HallmarkApplication.java
@@ -0,0 +1,13 @@
+package com.hfsystems.hallmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HallmarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HallmarkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hfsystems/hallmark/HallmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hfsystems.hallmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HallmarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
